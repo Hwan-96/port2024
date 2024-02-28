74a229313b33a4b72787fb11abe951324c3160d8
Hwan-96 <yunhwan0960@gmail.com>
2024-02-28 17:11:52 +0900

    폴더 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div>App</div>
-  );
+import HomeView from "./views/HomeView";
+
+const App = () =>{
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
