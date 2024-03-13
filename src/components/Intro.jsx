5195adc217a1f679cf9c099e1a5d18e3419ee016
Hwan-96 <yunhwan0960@gmail.com>
2024-03-13 18:15:09 +0900

    update 240313

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,101 +1,25 @@
 import React from "react";
 
-// const canvas = document.getElementById('canvas');
-// 	const ctx = canvas.getContext('2d');
-// 	canvas.width = window.innerWidth;
-// 	canvas.height = window.innerHeight;
-
-// 	let spots = [];
-// 	let hue = 0;
-
-// 	const mouse = {
-// 		x: undefined,
-// 		y: undefined
-// 	}
-
-// 	canvas.addEventListener('mousemove',function(e){
-// 		mouse.x = e.x;
-// 		mouse.y = e.y;
-// 		for(let i = 0; i < 3; i++){
-// 			spots.push(new Particle());
-// 		}
-// 	});
-
-// 	class Particle{
-// 		constructor(){
-// 			this.x = mouse.x;
-// 			this.y = mouse.y;
-// 			this.size = Math.random() * 2 + 0.3;
-// 			this.speedX = Math.random() * 2 - 1;
-// 			this.speedY = Math.random() * 2 - 1;
-// 			this.color = 'hsl(' + hue + ', 100%, 50%)';
-// 		}
-// 		update(){
-// 			this.x += this.speedX;
-// 			this.y += this.speedY;
-// 			if(this.size > 0.1) this.size -= 0.01;
-// 		}
-// 		draw(){
-// 			ctx.fillStyle = this.color;
-// 			ctx.beginPath();
-// 			ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-// 			ctx.fill();
-// 		}
-// 	}
-
-// 	function handlerParticle(){
-// 		for (let i = 0; i < spots.length; i++){
-// 			spots[i].update();
-// 			spots[i].draw();
-// 			for(let j = i; j < spots.length; j++){
-// 				const dx = spots[i].x - spots[j].x;
-// 				const dy = spots[i].y - spots[j].y;
-// 				const distance = Math.sqrt(dx * dx + dy * dy);
-// 				if(distance < 90){
-// 					ctx.beginPath();
-// 					ctx.strokeStyle = spots[i].color;
-// 					ctx.lineWidth = spots[i].size / 10;
-// 					ctx.moveTo(spots[i].x, spots[i].y);
-// 					ctx.lineTo(spots[j].x, spots[j].y);
-// 					ctx.stroke();
-// 				}
-// 			}
-// 			if(spots[i].size <= 0.3){
-// 				spots.splice(i, 1); i--;
-// 			}
-// 		}
-// 	}
-
-// 	function animate(){
-// 		ctx.clearRect(0,0, canvas.width, canvas.height);
-// 		handlerParticle();
-// 		hue++;
-// 		requestAnimationFrame(animate);
-// 	}
-
-// 	window.addEventListener('resize',function(){
-// 		canvas.width = window.innerWidth;
-// 		canvas.height = window.innerHeight;
-// 	})
-
-// 	window.addEventListener('mouseout', function(){
-// 		mouse.x = undefined;
-// 		mouse.y = undefined;
-// 	})
-// 	animate();
+import about from "../assets/img/about.png";
+import { introText } from "../constants"
 
 const Intro = () => {
-
 	return(
 		<section id="intro">
-			<canvas id="canvas"></canvas>
 			<div className="intro-inner">
-				<h1 className="main-title">Web developer</h1>
-				<h2>Yun Hwan Kim</h2>
+				<h1 className="intro-title">
+					<span>{introText.title[0]}</span>
+					<span>{introText.title[1]}</span>
+				</h1>
 				<div className="intro-text">
-					<div>Only I can change me life,</div>
-					<div>no one can do it for me.</div>
-					<div>-Carol Burnett-</div>
+					<ul className="text">
+						<li>{introText.desc[0]}</li>
+						<li>{introText.desc[1]}</li>
+						<li>{introText.desc[2]}</li>
+					</ul>
+					<div className="img">
+						<img src={ about } alt="about" />
+					</div>
 				</div>
 			</div>
 		</section>
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import { skillText } from "../constants";
+
 const Skill = () => {
-    return <div>Skill</div>;
+    return (
+      <section id="skill">
+        <div className="skill-inner">
+          <h2 className="skill-title">Challenge<em>나의 도전</em></h2>
+          <div className="skill-desc">
+            {skillText.map((skill,key)=>(
+              <div>
+                <span>{key+1}</span>
+                <h3>{skill.title}</h3>
+                <p>{skill.desc}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    );
 };
 
 export default Skill;
